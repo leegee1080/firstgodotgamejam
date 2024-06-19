3f54a7a44f7d425f1838d71a21f6af200538d41d
Mitchell Greene <mitchell.lee.greene@gmail.com>
2024-06-19 19:31:31 -0400

    got the classes working for changing menus

---
+++ code/main_menu_container.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+signal main_menu_signal_next_menu()
+
+
+func _on_play_button_button_up():
+	main_menu_signal_next_menu.emit()
+
+
+func _on_options_button_up():
+	print("options")
+
+
+func _on_quit_button_up():
+	print("quit")
--- code/menu_controller.gd
+++ code/menu_controller.gd
@@ -4,16 +4,30 @@ class_name MenuController
 
 enum Menus {Main, Options, Confirm, Pause, Credits}
 
+@export_category("Order is: Main, Options, Confirm, Pause, Credits")
 @export var menu_array: Array[Control]
 
 var menu_dict
+var current_menu
 
 func _ready():
 	
 	menu_dict = {
-	Menus.Main: menu_array[0],
-	Menus.Options: menu_array[1],
-	Menus.Confirm: menu_array[2],
-	Menus.Pause: menu_array[3],
-	Menus.Credits: menu_array[4],
-}
+		Menus.Main: menu_array[0],
+		Menus.Options: menu_array[1],
+		Menus.Confirm: menu_array[2],
+		Menus.Pause: menu_array[3],
+		Menus.Credits: menu_array[4],
+	}
+	
+	current_menu = menu_dict[Menus.Main]
+
+
+func change_menu(next_menu_enum):
+	current_menu.set_visible(false)
+	#menu_dict[next_menu_enum].visible = true
+	#current_menu = menu_dict[next_menu_enum]
+
+
+func _on_main_menu_container_main_menu_signal_next_menu():
+	change_menu(Menus.Main)
