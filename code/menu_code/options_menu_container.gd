45f59156141b2eaddb1b29cbde966e8c87d74096
Mitchell Greene <mitchell.lee.greene@gmail.com>
2024-07-02 21:21:02 -0400

    removed colors menu, its not needed based on the colors I am using

--- code/menu_code/options_menu_container.gd
+++ code/menu_code/options_menu_container.gd
@@ -6,9 +6,6 @@ signal options_menu_signal_next_menu(next_menu_int)
 
 
 
-func _on_colors_button_up():
-	print("color")
-
 
 func _on_volume_button_up():
 	print("volume")
@@ -17,3 +14,7 @@ func _on_volume_button_up():
 func _on_back_button_up():
 	options_menu_signal_next_menu.emit(0)
 	print("back")
+
+
+func _on_credits_button_up():
+	print("credits")
