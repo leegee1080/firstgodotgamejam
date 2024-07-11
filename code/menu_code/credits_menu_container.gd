f794b605e0f8b8318aae44ce4c8b92ce90eb306b
Mitchell Greene <mitchell.lee.greene@gmail.com>
2024-07-10 21:08:51 -0400

    started work on the credits scene

---
+++ code/menu_code/credits_menu_container.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+signal options_menu_signal_next_menu(next_menu_int)
+
+
+
+
+func _on_volume_button_up():
+	print("volume")
+
+
+func _on_back_button_up():
+	options_menu_signal_next_menu.emit(0)
+	print("back")
+
+
+func _on_credits_button_up():
+	print("credits")
